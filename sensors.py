370cdf0f89e9c7fe5bc789ca1af9e69c11a320d4
javi96 <josejaco@ucm.es>
2019-04-19 01:12:48 +0200

    Ahora si funciona bien

--- sensors.py
+++ sensors.py
@@ -5,35 +5,41 @@ from datetime import datetime
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-led = 21
-pir1 = 23
-pir2 = 20
+#led = 21
+pir1 = 20
+pir2 = 26
 
-time = list()
+time_sensor = list()
 
-GPIO.setup(led, GPIO.OUT) 
+#GPIO.setup(led, GPIO.OUT) 
 GPIO.setup(pir1, GPIO.IN, GPIO.PUD_DOWN) 
 GPIO.setup(pir2, GPIO.IN, GPIO.PUD_DOWN) 
 
 state = 0
 
 try:
-	sleep(5)
+	time.sleep(2)
 	print('config completed...')
+			
 	while True:
-		if state == 0: # reconocer primer objeto
-			if GPIO.input(pir1):
-				print('pir 1 detected')
-				time.append(datetime.now())
-				state += 1
-		elif state == 1: # reconocer segundo objeto
-			if GPIO.input(pir2):
-				print('pir 2 detected')
-				time.append(datetime.now())
-				state += 1
-		elif state == 2: # calcular infracción
+		if state == 0 and GPIO.input(pir1):
+			print('pir 1 detected', datetime.now())
+			time_sensor.append(1)
+			state += 1
+			time.sleep(2)
+		elif state == 1 and GPIO.input(pir2):
+			print('pir 2 detected', datetime.now())
+			time_sensor.append(2)
+			state += 1
+			time.sleep(2)
+		elif state == 2:
 			print('result: ')
-			print((time[1]-time[0]).seconds)
+			print(time_sensor[1]-time_sensor[0])
 			state = 0
-except: 
-	GPIO.cleanup([led, pir1, pir2])
+			time.sleep(2)
+except:
+	print('rompe')
+	GPIO.cleanup([pir1, pir2])
+
+
+# conexion de los pines de los sm --> vcc,gcc,out
